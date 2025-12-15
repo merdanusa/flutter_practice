7f0e58697b685763c1983a675c4a14644112897b
Merdan <wisew.tube@gmail.com>
2025-12-16 00:13:06 +0500

    init commit

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/start_screen.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.light(), home: StartScreen());
+  }
+}
