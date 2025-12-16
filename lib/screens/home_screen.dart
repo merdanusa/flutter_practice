904c4fc02899c20750a6233ccac79b0c0d940537
Merdan <wisew.tube@gmail.com>
2025-12-16 23:24:24 +0500

    init commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(margin: EdgeInsets.all(10), child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        
+      ],
+    ),);
   }
 }
