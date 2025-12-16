f33ea7b1f11981da5beaf1220c6721ad00cc97d5
Merdan <wisew.tube@gmail.com>
2025-12-16 23:18:22 +0500

    init commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/home_screen.dart';
 import 'package:flutter_application_1/screens/welcome_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter _router = GoRouter(
+  routes: [GoRoute(path: "/", builder: (context, state) => const HomeScreen(8))],
+);
 
 class App extends StatefulWidget {
   const App({super.key});
