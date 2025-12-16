1a3ccb469e6ff40c99a358e737aca7dd904e3874
Merdan <wisew.tube@gmail.com>
2025-12-16 23:33:17 +0500

    init commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,8 +19,24 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             TextField(
               decoration: InputDecoration(
+                labelText: 'New Task',
+                fillColor: Colors.grey[200],
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextButton.icon(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+              label: Text("Add new Task!", style: TextStyle(fontSize: 20)),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 0,
+                  vertical: 15,
                 ),
               ),
             ),
