5038974ca51aff727ce6fffd9acc0da3452daf4b
Merdan <wisew.tube@gmail.com>
2025-12-16 22:05:20 +0500

    init commit

--- lib/app.dart
+++ lib/app.dart
@@ -11,6 +11,10 @@ class _AppState extends State<App> {
   @override
   Widget build(context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(centerTitle: true, title: const Text('Home')),
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        
+      ],
+    );
+  }
+}
